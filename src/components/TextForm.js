36e6489e5461aaa1515817b34f78b35de0ca4958
Avani Anand <iamavanianand@gmail.com>
2024-06-15 12:43:04 +0530

    " lecture 9 completed -adding more logic to textutils "

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,9 +4,15 @@ export default function (props) {
 
 
     const handleUpClick=()=>{
-        console.log("uppercase was clicked" +text);
+        // console.log("uppercase was clicked" +text);
          let newText= text.toUpperCase();
          setText(newText)
+    }
+    const handleLwClick=()=>{
+        // console.log("uppercase was clicked" +text);
+        let newText=text.toLocaleLowerCase();
+        setText(newText)
+         
     }
 
     const handleonChange=(event)=>{     //because of this function we make changes in textarea then it invokes this function onchange function gives event so we use event in handleonChange and "event.target.value" it set the text we type in Textarea
@@ -14,19 +20,32 @@ export default function (props) {
         setText(event.target.value)  
     }
 
-    const[text, setText]=useState('enter text here !');//it means that the ("text ")value is( "enter text here" )but if we want to change its value then we use "setText" to change its value like setText("avani") now "text" value is "avani" because we can not change its value directly(like text=avni) we can only change  it by this function" usestate"
+    const[text, setText]=useState();//it means that the ("text ")value is( "enter text here" )but if we want to change its value then we use "setText" to change its value like setText("avani") now "text" value is "avani" because we can not change its value directly(like text=avni) we can only change  it by this function" usestate"
     // text="new text";  we cant set text directly thats why we use update function 
     //  setText("new text"); //this is corect way to change its value
 
     return (
-        <div>
 
-            <h1>{props.heading}</h1>
+        <>
+        <div className='container'>
+
+            <h2>{props.heading}</h2>
             <div className="mb-3">
                 <textarea className="form-control"  value={text} onChange={handleonChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleLwClick}>convert to Uppercase</button>
 
         </div>
+
+        <div className="container">
+            <h1>tour text summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters </p>
+            <p>{0.008 *text.split(" ").length }takes minutes to read</p>
+            <h2>preview  words</h2>
+            <p>{text}</p>
+        </div>
+
+        </>
     );
 }
